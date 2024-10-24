2132499404e7ff3db4561757fb23d8939196e422
korius kussa wenda <koriuswenda@gmail.com>
2024-10-24 14:35:36 +0700

    update

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -5,11 +5,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Blog</title>
+    <title>Halaman Blog</title>
 </head>
 
 <body>
     <h1>Halaman Blog</h1>
+    <ul>
+        <li>
+            <a href="https://facebook.com/wendakussa.wenebela">Facebook</a>
+        </li>
+
+        <li>
+            <a href="https://instagram.com/korius.wenda">Instagram</a>
+        </li>
+
+        <li>
+            <a href="https://youtube.com/korius.wenda">Instagram</a>
+        </li>
+    </ul>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum impedit commodi, animi voluptates velit aliquam
         excepturi ipsam nostrum voluptate suscipit labore ipsa iure minus doloribus est provident saepe sit tempora
         inventore facilis, corporis amet magni ab! Voluptatibus quos beatae, id, porro illum quam sequi, quaerat
